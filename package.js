0af71e778a92dec3d224fd766cbf6d9ec720e1d5
defc0n1 <fabiankliem@hotmail.com>
2016-09-29 18:36:05 +0200

    latest

--- package.js
+++ package.js
@@ -2,23 +2,38 @@ Package.describe({
   name: 'defc0n1:cart',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'fork of natestrauser cart',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/defc0n1/cart',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.4.1.1');
-  api.use('ecmascript');
-  api.mainModule('cart.js');
-});
+Package.on_use(function (api) {
+  api.versionsFrom("METEOR@0.9.0");
+  Npm.depends({ "stripe": "2.8.0" });
 
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('defc0n1:cart');
-  api.mainModule('cart-tests.js');
+  api.use([
+      'tracker@1.0.3',
+      'templating',
+      'session',
+      'amplify'
+    ]
+    , 'client');
+
+  api.use([
+    'iron:router@1.0.0', 'mongo@1.0.8', 'underscore', 'accounts-base', 'random'
+    ], ['server','client']);
+
+  
+  api.addFiles('lib/stripe/stripe_client.js', 'client');
+  api.addFiles('lib/stripe/stripe_checkout.js', 'client');
+  api.addFiles('lib/stripe/stripe_server.js', 'server');
+
+    api.add_files(['lib/both/environment.js'], ['client','server']);
+    api.add_files(['lib/client/cart.html','lib/client/cart.js'], 'client');
+    api.add_files(['lib/server/publications.js'], 'server');
+
+    api.export('Cart', ['client','server']);
 });
